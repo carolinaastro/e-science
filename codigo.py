76596e11ef2eb44c02e01f70b94ab4ed2e09b795
Ana Carolina <carolinaastro@on.br>
2016-04-13 14:33:37 -0300

    Pedi pra printar alguns arrays específicos dos dados

--- codigo.py
+++ codigo.py
@@ -5,3 +5,8 @@ print(type(data))
 print(data.shape)
 print('first value in data:', data[0, 0])
 print('middle value in data:', data[30, 20])
+print(data[0:4, 0:10])
+print(data[5:10, 0:10])
+small = data[:3, 36:]
+print('small is:')
+print(small)
