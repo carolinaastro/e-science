d077a9b61d245b080233c2384878c91228930f5c
Ana Carolina <carolinaastro@on.br>
2016-04-15 15:52:00 -0300

    Terminei a terceira atividade

--- codigo3.py
+++ codigo3.py
@@ -8,4 +8,26 @@ for i in saudation: #para os indices do string
     for c in i: #conteudo do indice
         my_list.extend(c) #a nova lista recebe os conteudos, formando uma nova lista
         
-print my_list  #printar nova lista      
+print my_list  #printar nova lista
+'''
+Tuples and exchanges
+Explain what the overall effect of this code is:
+
+left = 'L'
+right = 'R'
+
+temp = left #a variavel temp recebe o conteudo da variavel left, então temp = 'L'
+left = right #a variavel left recebe o conteudo da variavel right, então left = 'R'
+
+right = temp #a variavel right recebe o conteudo da variavel temp, então right = 'L'
+
+temp= 'L'
+left='R'
+right= 'F'
+Compare it to:
+
+left, right = right, left #answer= 'R''R'
+Do they always do the same thing? Which do you find easier to read?
+#Não fazem sempre a mesma coisa, a primeira forma é mais facil de ler
+
+'''      
