8d8018a2ba791aa82f70d0f552ecb4c674d1d4d3
Ana Carolina <carolinaastro@on.br>
2016-04-15 13:19:41 -0300

    Iniciei a parte 2 da atividade

---
+++ codigo2.py
@@ -0,0 +1,8 @@
+#programa 2 do software carpentry
+
+#From 1 to N: Using range, write a loop that uses range to print the first 3 natural numbers: 1,2,3
+
+import numpy
+
+for a in range(1, 4):
+    print(a)	
